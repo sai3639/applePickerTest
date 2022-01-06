2a36c046d6e87661698953b42ee432b0cca7e957
sai3639 <91225513+sai3639@users.noreply.github.com>
2022-01-06 15:21:47 -0600

    Add files via upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,122 @@
+﻿namespace apple3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.apple = new System.Windows.Forms.PictureBox();
+            this.ground = new System.Windows.Forms.PictureBox();
+            this.player = new System.Windows.Forms.PictureBox();
+            this.appleLabel = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.apple)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 60;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // apple
+            // 
+            this.apple.BackColor = System.Drawing.Color.Transparent;
+            this.apple.Image = global::apple3.Properties.Resources.New_Piskel1;
+            this.apple.Location = new System.Drawing.Point(469, 318);
+            this.apple.Name = "apple";
+            this.apple.Size = new System.Drawing.Size(32, 32);
+            this.apple.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.apple.TabIndex = 2;
+            this.apple.TabStop = false;
+            this.apple.Tag = "apple";
+            // 
+            // ground
+            // 
+            this.ground.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.ground.Image = global::apple3.Properties.Resources.ground_layer2;
+            this.ground.Location = new System.Drawing.Point(-6, 358);
+            this.ground.Name = "ground";
+            this.ground.Size = new System.Drawing.Size(805, 92);
+            this.ground.TabIndex = 1;
+            this.ground.TabStop = false;
+            this.ground.Tag = "ground";
+            // 
+            // player
+            // 
+            this.player.BackColor = System.Drawing.Color.Transparent;
+            this.player.Image = global::apple3.Properties.Resources.New_Piskel__15_;
+            this.player.Location = new System.Drawing.Point(320, 294);
+            this.player.Name = "player";
+            this.player.Size = new System.Drawing.Size(64, 64);
+            this.player.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.player.TabIndex = 0;
+            this.player.TabStop = false;
+            // 
+            // appleLabel
+            // 
+            this.appleLabel.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.appleLabel.Location = new System.Drawing.Point(604, 9);
+            this.appleLabel.Name = "appleLabel";
+            this.appleLabel.Size = new System.Drawing.Size(184, 82);
+            this.appleLabel.TabIndex = 3;
+            this.appleLabel.Text = "Apples: ";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.appleLabel);
+            this.Controls.Add(this.apple);
+            this.Controls.Add(this.ground);
+            this.Controls.Add(this.player);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.apple)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.player)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox player;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.PictureBox ground;
+        private System.Windows.Forms.PictureBox apple;
+        private System.Windows.Forms.Label appleLabel;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,236 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace apple3
+{
+    public partial class Form1 : Form
+    {
+
+        bool goLeft, goRight, sword, swordSwing;
+        bool hold = true;
+        int playerSpeed = 1;
+        bool jump = false;
+        bool run;
+
+        bool playerPosition;
+        int force;
+        int jumpSpeed;
+        int swing = 0;
+        int apples = 0;
+        int sprint = 0;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+
+            if (e.KeyCode == Keys.Left&hold)
+            {
+                goLeft = true;
+                hold = false;
+                player.Image = Properties.Resources.left;
+                sprint++;
+                if (sprint < 2)
+                {
+                    playerSpeed = 1;
+                }
+                if (sprint == 2)
+                {
+                    run = true;
+                    playerSpeed = 3;
+                }
+            
+            }
+            if (e.KeyCode == Keys.Right&hold)
+            {
+                goRight = true;
+                hold = false;
+                player.Image = Properties.Resources.New_Piskel;
+                sprint++;
+                if (sprint < 2)
+                {
+                    playerSpeed = 1;
+                }
+                if (sprint == 2)
+                {
+                    run = true;
+                    playerSpeed = 3;
+                }
+            }
+            if (e.KeyCode == Keys.Up && jump == false)
+            {
+                if (playerPosition == false)
+                {
+                    jump = true;
+                    player.Image = Properties.Resources.jump2;
+                }
+                else if (playerPosition == true)
+                {
+                    jump = true;
+                    player.Image= Properties.Resources.New_Piskel__22_;
+                }
+                
+            }
+            if (e.KeyCode == Keys.Space)
+            {
+                if (playerPosition == false)
+                {
+                    sword = true;
+                    player.Image = Properties.Resources.sword2;
+                    swing++;
+                    if (swing == 2)
+                    {
+                        swordSwing = true;
+                        player.Image = Properties.Resources.New_Piskel__20_;
+                    }
+                }
+                else if (playerPosition == true)
+                {
+                    sword = true;
+                    player.Image = Properties.Resources.New_Piskel__19_;
+                    swing++;
+                    if (swing == 2)
+                    {
+                        swordSwing = true;
+                        player.Image = Properties.Resources.New_Piskel__21_;
+                    }
+                }
+
+                
+            }
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Left&!hold)
+            {
+                goLeft = false;
+                hold = true;
+                run = false;
+                player.Image= Properties.Resources.New_Piskel__18_;
+            }
+            if (e.KeyCode == Keys.Right&!hold)
+            {
+                goRight = false;
+                hold = true;
+                run = false;
+                player.Image = Properties.Resources.New_Piskel__15_;
+            }
+            if (jump == true)
+            {
+                if (playerPosition == false)
+                {
+                    jump = false;
+                    player.Image = Properties.Resources.jump1;
+                }
+                else if (playerPosition == true)
+                {
+                    jump = false;
+                    player.Image = Properties.Resources.New_Piskel__18_;
+                }
+               
+            }
+            if (e.KeyCode == Keys.Space && goRight == true || e.KeyCode == Keys.Space && goLeft == true)
+            {   
+                if (goRight == true)
+                {
+                    sword = false;
+                    swordSwing= false;
+                    player.Image = Properties.Resources.sword1;
+                }
+                else if (goLeft == true)
+                {
+                    sword = false;
+                    swordSwing = false;
+                    player.Image = Properties.Resources.New_Piskel__18_;
+                }
+               
+                
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            player.Top += jumpSpeed;
+            if (goLeft == true)
+            {
+                player.Left -= playerSpeed;
+                playerPosition = true;
+            }
+            if (goRight == true)
+            {
+                player.Left += playerSpeed;
+                playerPosition = false;
+            }
+
+            if (jump == true)
+            {
+                jumpSpeed = -8;
+                force -= 1;
+
+            }
+            else if (jump == true && force < 0)
+            {
+                jump = false;
+            }
+            else
+            {
+                jumpSpeed = 10;
+            }
+
+            if (player.Bounds.IntersectsWith(ground.Bounds))
+            {
+                player.Top = ground.Top - player.Height;
+                force = 8;
+                ground.BringToFront();
+
+            }
+            if (swing == 2)
+            {
+                swing = 0;
+            }
+            
+            
+
+            if (player.Bounds.IntersectsWith(apple.Bounds) && apple.Visible == true)
+            {
+                apple.Visible = false;
+                apples++;
+                appleLabel.Text = "Apples: " + apples;
+            }
+
+            if (sprint == 2)
+            {
+                sprint = 0;
+            }
+       
+      
+
+
+
+
+
+    }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+           
+
+
+        }
+    }
+}
